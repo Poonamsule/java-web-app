02426be6b722e1d22f7ba7f24ca85771ada22376
Poonamsule <poonamsonwalkar@gmail.com>
2020-08-03 22:13:55 +0530

    First commit

---
+++ src/main/java/com/javawebapp/demo/JavaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.javawebapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javawebapp/demo/JavaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawebapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
